d9efafde128b1db825617097a0832bac328d83cc
Andrew77747 <andrew-walker@yandex.ru>
2021-10-15 17:55:42 +0300

    Begining

---
+++ Solution of WebShop/WebShop.Tricentis.Framework/PageObject/MainPage.cs
@@ -0,0 +1,17 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebShop.Tricentis.Framework.PageObject
+{
+    class MainPage
+    {
+
+        #region MapsOfElements
+        private readonly By _login = By.CssSelector("");
+        #endregion
+    }
+}
