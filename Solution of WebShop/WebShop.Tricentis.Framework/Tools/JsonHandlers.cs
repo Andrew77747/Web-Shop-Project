1c591618840ce14b2bb776e09b0b4688f32b2d63
Andrew77747 <andrew-walker@yandex.ru>
2021-12-23 14:54:37 +0300

    JsonHandlers added

---
+++ Solution of WebShop/WebShop.Tricentis.Framework/Tools/JsonHandlers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace WebShop.Tricentis.Framework.Tools
+{
+    public class JsonHandlers
+    {
+        public void JsonReader(Appsettings model)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                .AddJsonFile("appsettings.json").Build();
+
+            var section = config.GetSection(nameof(Appsettings));
+            var appsettingsModel = section.Get<Appsettings>();
+            model = appsettingsModel;
+
+            Console.WriteLine(model.Url);
+            Console.WriteLine(model.Driver);
+            Console.WriteLine(model.Timeout);
+            Console.WriteLine(model.Driver);
+        }
+    }
+}
