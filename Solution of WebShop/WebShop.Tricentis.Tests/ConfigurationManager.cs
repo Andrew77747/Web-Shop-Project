06c268e7bc0a51176b1df0e8fbe915774c62e9ed
Andrew77747 <andrew-walker@yandex.ru>
2022-01-21 14:11:04 +0300

    Appsettings goes to shopping cart. Partly done

---
+++ Solution of WebShop/WebShop.Tricentis.Tests/ConfigurationManager.cs
@@ -0,0 +1,19 @@
+﻿using Infrastructure.Settings;
+using Infrastructures.Settings;
+using Microsoft.Extensions.Configuration;
+
+namespace WebShop.Tricentis.Tests
+{
+    public class ConfigurationManager
+    {
+        public Appsettings GetSettings()
+        {
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json").Build();
+
+            var settings = config.Get<Appsettings>();
+
+            return settings;
+        }
+    }
+}
