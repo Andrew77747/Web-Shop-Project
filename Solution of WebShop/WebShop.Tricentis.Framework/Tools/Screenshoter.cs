0f1b1a7884ace9d5727a3d98147904d6c1acc9c2
Andrew77747 <andrew-walker@yandex.ru>
2022-02-01 19:44:56 +0300

    Commit 01.02 evening

--- Solution of WebShop/WebShop.Tricentis.Framework/Tools/Screenshoter.cs
+++ Solution of WebShop/WebShop.Tricentis.Framework/Tools/Screenshoter.cs
@@ -1,14 +1,28 @@
-﻿using System;
+﻿using System.IO;
+using NUnit.Framework;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Support.Extensions;
 
 namespace WebShop.Tricentis.Framework.Tools
 {
     public class Screenshoter
     {
-
-        public void TakeScreenshot(string testName)
+        private IWebDriver _driver;
+        public Screenshoter(IWebDriver driver, string testName)
         {
-            _driver.TakeScreenshot().SaveAsFile(GetFileName(path, testName)); 
+            _driver = driver;
+        }
+
+        public string GetFileName(string path, string testName)
+        {
+            path = Path.GetFullPath(@"\Screenshots");
+            return path;
+        }
+
+        public void TakeScreenshot(string path, string testName)
+        {
+            //_driver.TakeScreenshot().SaveAsFile(GetFileName(path, testName));
+            _driver.TakeScreenshot().SaveAsFile(@"\Screenshots", "testName");
         }
     }
 }
