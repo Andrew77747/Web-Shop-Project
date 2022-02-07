35db738bbfceb60af0e4f7387c1784324fd1b788
Andrew77747 <andrew-walker@yandex.ru>
2022-02-07 15:20:21 +0300

    Screenshoter doesn't work

--- Solution of WebShop/WebShop.Tricentis.Tests/Handlers/ScenarioHandler.cs
+++ Solution of WebShop/WebShop.Tricentis.Tests/Handlers/ScenarioHandler.cs
@@ -1,6 +1,6 @@
-﻿using System;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using OpenQA.Selenium;
+using System;
 using TechTalk.SpecFlow;
 using WebShop.Tricentis.Framework.Tools;
 
@@ -9,13 +9,21 @@ namespace WebShop.Tricentis.Tests.Handlers
     [Binding]
     public class ScenarioHandler
     {
+        //public WebDriverManager Manager;
+
+        //public ScenarioHandler()
+        //{
+        //    Manager = new WebDriverManager();
+        //}
+
         [AfterScenario]
         public void Error(ScenarioContext context, IWebDriver driver)
         {
             if (context.TestError != null)
             {
-                var screen = new Screenshoter(driver, TestContext.CurrentContext.Test.Name);
-                TestContext.AddTestAttachment(screen.Path);
+                var screenshot = new ScreenshotMaker(driver, TestContext.CurrentContext.Test.Name);
+                Console.WriteLine("The screen shot was made into " + screenshot.Path);
+                TestContext.AddTestAttachment(screenshot.Path);
             }
         }
     }
